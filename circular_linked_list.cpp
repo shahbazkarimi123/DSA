8f67b9b54e198f17cf23dd27c6441b35e8e963b1
shahbazkarimi123 <shahbazkarimi123@gmail.com>
2021-07-04 04:19:01 +0530

    circular linked list added

---
+++ circular_linked_list.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+class Node
+{
+    public:
+        int data;
+        Node* next;
+
+        Node(int val)
+        {
+            data=val;
+            next=NULL;
+        }
+};
+void insert_at_starting(Node* &head,int val)
+{
+    Node* n=new Node(val);
+    if(head==NULL)
+    {
+        n->next=n;
+        head=n;
+        return;
+    }
+    
+    Node* temp=head;
+    while(temp->next!=head)
+    {
+        temp=temp->next;
+    }
+    temp->next=n;
+    n->next=head;
+    head=n;
+
+}
+void insert_at_end(Node* &head,int val)
+{
+    if(head==NULL)
+    {
+        insert_at_starting(head,val);
+        return;
+    }
+    Node* n=new Node(val);
+    Node* temp=head;
+    while(temp->next!=head)
+    {
+        temp=temp->next;
+    }
+    temp->next=n;
+    n->next=head;
+}
+void display(Node* head)
+{
+    Node* temp=head;
+    do
+    {
+        std::cout<<temp->data<<" ";
+        temp=temp->next;
+    } while (temp!=head);
+    
+    while(temp!=head)
+    {
+        std::cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+    std::cout<<std::endl;
+
+}
+int main() 
+{
+    Node* head=NULL;
+    insert_at_end(head,9);
+    insert_at_end(head,6);
+    insert_at_end(head,3);
+    display(head);
+}
