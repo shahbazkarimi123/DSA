7bd7b527d5cd6cc3fdb08b31b1db0944a02ad29e
shahbazkarimi123 <shahbazkarimi123@gmail.com>
2021-07-22 00:51:24 +0530

    not completed search bst

---
+++ searchInBst.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+class Node
+{
+    public:
+        int data;
+        Node* left;
+        Node* right;
+
+        Node(int val)
+        {
+            data=val;
+            left=NULL;
+            right=NULL;
+        }
+};
+
+void inorder(Node* root,int val)
+{
+
+}
+int main()
+{
+    Node* root;
+    
+}
